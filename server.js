bebe65ba47d1b7fd5d517e9ce390c101158c1e35
Derek <derekdenhartigh@gmail.com>
2019-06-05 20:29:52 -0400

    think its all finished here, just need to test w/ postman

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const express = require('express'); // imports the express module
-const router = require("./routing.js"); // imports router from my routing.js file
+const router = require("./user-routes.js"); // imports router from my routing.js file
 const app = express(); // creates an express application
 app.use(express.json());  // tells the api to serve up data in json
 app.use("/", router); // indicates what symbol to route with?
-app.use(express.static("./public")); // serves static files from this directory
+// app.use(express.static("./public")); // serves static files from this directory
 const port = 3000; // sets port #
 app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
 // tells server to listen @ specific port # & makes a link in console
